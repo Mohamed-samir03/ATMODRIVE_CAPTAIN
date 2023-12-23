1929dfb71f0a7d4702710856e2d5601a5e662f2e
mo samir <smyr03130@gmail.com>
2023-12-23 19:28:55 +0200

    add setting nav graph and captain profile fragment

---
+++ app/src/main/java/com/mosamir/atmodrivecaptain/features/setting/presentation/common/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.mosamir.atmodrivecaptain.features.setting.presentation.common
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mosamir.atmodrivecaptain.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
---
+++ app/src/main/java/com/mosamir/atmodrivecaptain/features/setting/presentation/fragment/CaptainProfileFragment.kt
@@ -0,0 +1,42 @@
+package com.mosamir.atmodrivecaptain.features.setting.presentation.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mosamir.atmodrivecaptain.R
+import com.mosamir.atmodrivecaptain.databinding.FragmentCaptainProfileBinding
+
+class CaptainProfileFragment : Fragment() {
+
+    private var _binding: FragmentCaptainProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentCaptainProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
