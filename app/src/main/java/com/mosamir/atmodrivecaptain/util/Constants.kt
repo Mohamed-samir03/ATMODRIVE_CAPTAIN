ac817e57f54fece4a574086d3a3d990f2ac6f5fd
Mohamed Samir <81251707+Mohamed-samir03@users.noreply.github.com>
2024-01-25 20:16:08 +0200

    Atmodrive Captain

--- app/src/main/java/com/mosamir/atmodrivecaptain/util/Constants.kt
+++ app/src/main/java/com/mosamir/atmodrivecaptain/util/Constants.kt
@@ -6,8 +6,8 @@ class Constants {
 
     companion object{
 
-        const val BASE_URL = "https://s1.drive.api.atmosphere.solutions/api/v1/captains/"
-        const val UPLOAD_FILES_BASE_URL = "https://s1.drive.dashboard.atmosphere.solutions/"
+        const val BASE_URL = "BASE_URL"
+        const val UPLOAD_FILES_BASE_URL = "UPLOAD_FILES_BASE_URL"
 
 
         const val VEHICLE_IMAGE_PATH = "captains"
